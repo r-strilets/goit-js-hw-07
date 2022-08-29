2624a1e04e84bc4fa54e3e69dcfa632ee01947e3
r-strilets <str.komar@gmail.com>
2022-08-29 11:12:30 +0300

    add part of Task_2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї. Використовуй готовий код з першого завдання.
+// Підключення скрипту і стилів бібліотеки, використовуючи CDN сервіс cdnjs. Необхідно додати посилання на два файли: simple-lightbox.min.js і simple-lightbox.min.css.
+// Ініціалізація бібліотеки після створення і додання елементів галереї у div.gallery. Для цього ознайомся з документацією SimpleLightbox - насамперед секції «Usage» і «Markup».
+// Подивися в документації секцію «Options» і додай відображення підписів до зображень з атрибута alt. Нехай підпис буде знизу і з'являється через 250 мілісекунд після відкриття зображення.
+
+/* <div class="gallery__item">
+  <a class="gallery__item" href="large-image.jpg">
+    <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+  </a>
+</div>; */
+const gallery = document.querySelector(".gallery");
+
+function createGallery(arrayOfImages) {
+  const newGalleryItems = arrayOfImages
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+ `;
+    })
+    .join("");
+  return newGalleryItems;
+}
+gallery.innerHTML = createGallery(galleryItems);
+console.log(gallery);
+
+let galleryBox = new SimpleLightbox(".gallery a");
+galleryBox.on("show.simplelightbox", function () {});
